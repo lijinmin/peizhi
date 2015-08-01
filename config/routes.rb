4c77eefc4522635fd840f80df033347ebd4828b4
lijinmin <lijinmin@xjgreat.com>
2015-08-01 11:19:28 +0800

    m

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :wechats
-  get '/' => 'WelcomeController#indexs'
+  # get '/' => 'WelcomeController#indexs'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
